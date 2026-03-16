f957452bff0f5902637c1040bd496a4a7f963587
Hexa <syswarefun@gmail.com>
2026-03-17 06:42:38 +0800

    Add files via upload

--- offsets.rs
+++ offsets.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-13 23:06:11.282581500 UTC
+// 2026-03-16 22:40:29.102373900 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
--- vphysics2_dll.cs
+++ vphysics2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-13 23:06:11.282581500 UTC
+// 2026-03-16 22:40:29.102373900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: vphysics2.dll
